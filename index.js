b40300dfc9aeb2eae1eb6c1b3c4cbaabdc6f3970
kahveciderin <kahveciderin@gmail.com>
2022-04-18 22:56:35 +0300

    add required classes

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const GRID_SIZE = 16;
+
+
+class Cell{
+    
+}
+
+class WaveFunctionCollapse{
+
+}
+
 window.addEventListener('load', () => {
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i < GRID_SIZE; i++){
         let previewRow = document.createElement('div');
         previewRow.classList.add('preview-row');
         previewRow.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        for(let j = 0; j < 5; j++){
+        for(let j = 0; j < GRID_SIZE; j++){
             let previewCell = document.createElement('div');
             previewCell.classList.add('preview-cell');
             previewCell.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
